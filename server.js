ec73c35f66f1bd8de0f1a5540317b70bd6de1c61
Tegar Wibisana <95567621+ybgara@users.noreply.github.com>
2024-05-05 13:10:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
 const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const compression = require('compression');
 const app = express();
-
-// Gunakan middleware untuk logging dan kompresi
-app.use(morgan('dev'));
-app.use(compression());
-
-// Gunakan template engine EJS
-app.set('view engine', 'ejs');
-
-// Tambahkan file statis di folder 'public'
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Tambahkan beberapa rute tambahan
 app.get('/', (req, res) => {
-    res.render('index', { message: 'Hallo Ini Adalah Customer FE Version 1.0.0' });
+res.send('Hello from App Engine!');
 });
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-// Jalankan server
+// Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
+console.log(`Server listening on port ${PORT}...`);
 });
